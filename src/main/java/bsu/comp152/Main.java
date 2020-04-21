f197d9d095bfb4130f0abf8aa1ad115d8607f681
GoldenExperienceRequiem4 <dciobanu@student.bridgew.edu>
2020-04-20 21:33:29 -0400

    April 20th

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,6 +1,7 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.geometry.Pos;
@@ -8,11 +9,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.nio.file.Files;
+
 
 
 public class Main extends Application {
@@ -29,9 +29,19 @@ public class Main extends Application {
         promptTextField = new TextField();
         Button clickButton = new Button("Click");
         clickButton.setOnAction(new ClickButtonHandler());
-        VBox vbox = new VBox(10,promptLabel, clickButton);
-        Scene scene = new Scene(vbox, 300, 300);
-
+        VBox vbox = new VBox(10, promptLabel, promptTextField);
+        Scene scene = new Scene(vbox, 200, 200);
+        vbox.setAlignment(Pos.CENTER);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Comp 152 Project 4");
+        primaryStage.show();
 
     }
 }
+class ClickButtonHandler implements EventHandler<ActionEvent> {
+            public void handle(ActionEvent event) {
+                TextArea textArea = new TextArea();
+                textArea.setPrefColumnCount(20);
+                textArea.setPrefRowCount(20);
+            }
+        }
